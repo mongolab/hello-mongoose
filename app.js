483e1d4fc1a181e694a3336a07bb9a55e4afdfa1
Ben Wen <github@benwen.com>
2012-11-17 00:02:26 -0800

    Initial commit for hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku

---
+++ app.js
@@ -0,0 +1,133 @@
+//
+// hello-mongoose: MongoDB with Mongoose on Node.js example on Heroku.
+// Mongoose is a object/data mapping utility for the MongoDB database.
+//
+
+// by Ben Wen with thanks to Aaron Heckmann
+
+//
+// Copyright 2012 ObjectLabs Corp.  
+// ObjectLabs operates MongoLab.com a MongoDb-as-a-Service offering
+//
+// MIT Licensed
+//
+
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation files
+// (the "Software"), to deal in the Software without restriction,
+// including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software,
+// and to permit persons to whom the Software is furnished to do so,
+// subject to the following conditions:  
+
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software. 
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+// BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+// ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+// CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE. 
+
+//
+// Preamble
+var http = require ('http');	     // For serving a basic web page.
+var mongoose = require ("mongoose"); // The reason for this demo.
+
+// Here we find an appropriate database to connect to, defaulting to
+// localhost if we don't find one.  
+var uristring = 
+  process.env.MONGODB_URI || 
+  process.env.MONGOLAB_URI || 
+  process.env.MONGOHQ_URL || 
+  'mongodb://localhost/HelloMongoose';
+
+// The http server will listen to an appropriate port, or default to
+// port 2000.
+var theport = process.env.PORT || 5000;
+
+// MongoDB's default is for speed over safety on writes.  Most
+// applications should prefer safety over speed, so that's what 
+// we use below.
+var mongoOptions = { db: { safe: true }};
+
+// Makes connection asynchronously.  Mongoose will queue up database
+// operations and release them when the connection is complete.
+mongoose.connect(uristring, mongoOptions, function (err, res) {
+  if (err) { 
+    console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+    console.log ('Succeeded connected to: ' + uristring);
+  }
+});
+
+// This is the schema.  Note the types, validation and trim
+// statements.  They enforce useful constraints on the data.
+var userSchema = new mongoose.Schema({
+  name: {
+    last: String,
+    first: { type: String, trim: true }
+  },
+  age: { type: Number, min: 0}
+});
+
+// Instantiating an instance of the model, opening (or creating, if
+// nonexistent) the 'PowerUsers' collection in the MongoDB database
+var PUser = mongoose.model('PowerUsers', userSchema);
+
+// Creating one user.
+var johndoe = new PUser ({
+  name: { last: 'John', first: '  Doe   ' },
+  age: Math.floor(Math.random()*91+1)
+});
+
+// Saving it to the database.  If you restart and run this code
+// multiple times, you will see multiple documents with random ages.
+johndoe.save();
+
+// In case the browser connects before the database is connected, the
+// user will see this message.
+var found = ['DB Connection not yet established.  Try again later.  Check the console output for error messages if this persists.'];
+
+// Create a rudimentary http server.  (Note, a real web application
+// would use a complete web framework and router like express.js). 
+// This is effectively the main interaction loop for the application. 
+// As new http requests arrive, the callback function gets invoked.
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  // Lets find all the documents
+  PUser.find({}).exec(function(err, result) { 
+    if (!err) { 
+      res.end(html1 + result.length +  html2 + JSON.stringify(result, undefined, 2) + html3);
+    } else {
+      res.end('Error in query. ' + err)
+    };
+  });
+}).listen(theport);
+
+// Tell the console we're getting ready.
+// The listener in http.createServer should still be active after these messages are emitted.
+console.log('http server will be listening on port %d', theport);
+console.log('CTRL+C to exit');
+
+// DONE.
+
+//
+// House keeping.
+
+//
+// The rudimentary HTML content in three pieces.
+var html1 = '<title> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </title> \
+<head> \
+<style> body {color: #394a5f; font-family: sans-serif} </style> \
+</head> \
+<h1> hello-mongoose: MongoLab MongoDB Mongoose Node.js Demo on Heroku </h1> \
+<br\> \
+There are: ';
+var html2 = ' documents. <br\>  <br\> <h2> Documents in MonogoDB database </h2> <pre><code>';
+var html3 = '</code></pre> <br\> <br\> <center><i> Demo code available at <a href="http://github.com/mongolab/hello-mongoose">github.com</a> </i></center>';
+
+
